630f1cee639dae0614cb3b8993c7859452b52237
okamurayasuyuki <haradashinya@gmail.com>
2013-05-23 10:32:31 +0900

    fix

--- client/js/ctrls/users_controller.js
+++ client/js/ctrls/users_controller.js
@@ -9,8 +9,6 @@ function UserCtrl($scope,$http){
 
       $http({method: 'GET',url: $scope.url+"/tags"})
         .success(function(data,status){
-
-
           $scope.tags = data.tags;
         })
       .error(function(d,s){
@@ -18,8 +16,19 @@ function UserCtrl($scope,$http){
       });
   };
 
-  $scope.find = function(t){
-    console.log(t);
+  // tag内で全文検索
+  $scope.renderTags = function(kwd){
+    console.log(kwd);
+
+    $http({method: 'POST',url: $scope.url+"/search/" + kwd})
+      .success(function(data,status){
+        console.log(data);
+
+        $scope.linkItems = data.data;
+      })
+    .error(function(d,s){
+      console.log(s);
+    });
   };
 
 
